6eb5b17487b96dcdd272c4152a4ee74d3283e880
HMBL98 <hugomichelbl@gmail.com>
2020-11-18 20:05:11 -0600

    Update ClaseRMI.java

--- ClaseRMI.java
+++ ClaseRMI.java
@@ -27,7 +27,7 @@ public class ClaseRMI extends UnicastRemoteObject implements InterfaceRMI
   }
   public int[][] multiplica_matrices(int[][] A,int[][] B) throws RemoteException
 	{
-		int N = A.length;
+		int N = A.length*2;
 		int[][] C = new int[N/2][N/2];
 		for (int i = 0; i < N/2; i++)
 			for (int j = 0; j < N/2; j++)
